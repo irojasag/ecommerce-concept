f4a4539ea6bc96de33177f5b19a2b58ff2a939f9
Ignacio Rojas Aguilar <imoon521@gmail.com>
2020-12-02 22:50:02 -0600

    Loading Screen Module and component Ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loading',
+    loadChildren: () =>
+      import('./pages/loading/loading.module').then((m) => m.LoadingModule),
+  },
+  { path: '**', redirectTo: 'loading' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
